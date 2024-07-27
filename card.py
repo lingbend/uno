1058bad3745535d0f31dd59ded2efc546ceeba9f
lingbend <lingbend@gmail.com>
2024-07-27 13:42:58 -0600

    1st draft card class

---
+++ card.py
@@ -0,0 +1,17 @@
+
+class Card:
+
+    ## reverse, draw_2, wild, wild_draw_4, skip
+
+    def __init__(self, color, type, deck, discard):
+        self.color = color
+        self.type = type
+        self.deck = deck
+        self.discard = discard
+
+    def play_card(self, type): # is used by play_card function for player
+        discard.add_card(self) # calls add card function on discard deck object
+        
+        if 
+        # check type
+        # if type not number, do action
