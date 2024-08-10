3ee954f830da6050bacd5a8098c97b8206a80d02
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2024-08-10 14:17:46 -0600

    refactor uno

--- Uno.py
+++ Uno.py
@@ -5,6 +5,8 @@ import game
 import player
 import render
 import update
+
+# Initialize The game
 pygame.init()
 g = game.Game()
 g.players.append(player.Player("Steve", 0))
@@ -12,31 +14,21 @@ g.players.append(player.Player("Chris", 1))
 g.players.append(player.Player("John", 2))
 g.deal_inital_cards_to_players()
 
-base_font = pygame.font.Font(None, 32)
-#create the screen
+# create the screen
 w = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-
-
-rect = pygame.Rect(200, 200, 140, 32) 
-user_text = ''
-
-color_active = pygame.Color('lightskyblue3') 
-  
-# color_passive store color(chartreuse4) which is 
-# color of input box. 
-color_passive = pygame.Color('chartreuse4') 
-color = color_passive 
-#title
+# set title
 pygame.display.set_caption("A Simple Game of Uno")
 
 
 while True:
+    # Render the cards
     clickable_cards, deck_rect = render.render(w, g)
-    # Input (with update inside)
+
+    # Get Input (with update inside)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             update.update(g, event, clickable_cards, deck_rect)
-        #ways to quit the game
+        # Ways to quit the game
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
@@ -45,4 +37,5 @@ while True:
                 pygame.quit()
                 sys.exit()
 
+    # Finish Game Loop
     pygame.display.flip()
